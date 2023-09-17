e548cde9fef43b67612fba025171cac0c58410f3
PaulNarvik <111660098+PaulNarvik@users.noreply.github.com>
2023-09-17 15:52:46 +0200

    Amélioration du labyrinthe

--- scripts/maze.py
+++ scripts/maze.py
@@ -10,14 +10,33 @@ class Maze:
 
     def new_maze(self, size):
         maze = []
+
+        # Valeurs choisis arbitrairement
         wall = 1
         empty = 0
+
         for i in range(size[0]):
+            # Alternance lignes complètes, creuses
             if i % 2 == 0:
                 line = [wall for i in range(size[1])]
             else:
                 line = [wall if i % 2 == 0 else empty for i in range(size[1])]
-                if line[-1] == empty:
-                    line.append(wall)
+
+            # Cas où la ligne finit par 0
+            if size[1] % 2 == 0:
+                line.append(wall)
+
             maze.append(line)
+
+        # Si la dernière ligne n'est pas complète
+        if size[0] % 2 == 0:
+            # Selon si la dimension x est paire ou impaire la ligne ne doit pas faire la même longueur
+            if size[1] % 2 == 0:
+                maze.append([wall for i in range(size[1] + 1)])
+            else:
+                maze.append([wall for i in range(size[1])])
+
         return maze
+    
+    def draw(self):
+        pass
